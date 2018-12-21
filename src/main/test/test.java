9dc907c0d51a940d0641e5dacc2ff7af344cc30a
Moon <pz13035085017@gmail.com>
2018-12-21 10:57:47 +0800

    init

--- src/main/test/test.java
+++ src/main/test/test.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"spring_mvc.xml","classpath:appContext.xml"})
+@ContextConfiguration(locations = {"classpath:spring_mvc.xml","classpath:appContext.xml"})
 public class test{
     @Autowired
     SqlSessionFactory sqlSessionFactory;
