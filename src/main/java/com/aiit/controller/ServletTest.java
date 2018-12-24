0e7e112be205b335efd284638a81b02b4350bf71
pz13035085017@gmail.com <pz13035085017@gmail.com>
2018-12-24 08:11:45 +0800

    init

--- src/main/java/com/aiit/controller/ServletTest.java
+++ src/main/java/com/aiit/controller/ServletTest.java
@@ -3,14 +3,15 @@ package com.aiit.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.io.PrintWriter;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ServletTest {
 
     @RequestMapping(value = "servletTest.action",method = RequestMethod.GET)
-    public void test(PrintWriter out){
-        out.print("success！呵呵呵");
+    @ResponseBody
+    public String test(){
+      //  out.print("success！呵呵呵");
+        return "呵呵呵";
     }
 }
